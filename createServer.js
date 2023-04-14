1531213608b125bb8aba97fb5b58a39ceeb9d797
Shashank Dubey <allyouget14@gmail.com>
2023-04-14 18:16:35 +0530

    We can pass HTML Tags with it

--- createServer.js
+++ createServer.js
@@ -1,14 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('My name is Shashank Dubey');
+    res.write('<p>My name is Shashank Dubey</p>');
     res.write('\nI am from Lucknow, Uttar Pradesh');
     res.end();
+    // console.log(req.url, req.method);
 });
 
-server.listen(8080, '127.0.0.1', () => {
+server.listen(8080, 'localhost', () => {
     console.log("Listening to the port 8080");
 });
 
-const response = http.STATUS_CODES;
-console.log(response);
+// const response = http.STATUS_CODES;
+// console.log(response);
